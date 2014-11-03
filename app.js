2b33e81dcc11abce1413481690f4ff85f812635f
Gleb Rurenko <grurenko@gmail.com>
2014-11-03 22:47:19 +0400

    first dict generator

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var sdch = require('sdch');
 var connectSdch = require('connect-sdch');
 var config = require('config-node')();
 var mkdirp = require('mkdirp');
+var exec = require('child_process').exec;
 
 var app = express();
 
@@ -21,6 +22,10 @@ var dicts = [
     data: fs.readFileSync(config.dictionaryFile)
   })
 ]
+var domainHits = new Array(config.domains.length);
+for (var i = 0; i < domainHits.length; i++) {
+    domainHits[i] = 0
+}
 // Создаем хранилище словарей
 var storage = new connectSdch.DictionaryStorage(dicts);
 
@@ -43,10 +48,7 @@ app.use(logger('common', { stream: proxyLog }
 app.use(logger(('":method :hostname";Avail-Dictionary:[:req[Avail-Dictionary]];'
                 + ':status;Get-Dictionary:[:res[Get-Dictionary]];'
                 + 'Content-type:[:res[content-type]];Content-Encoding:[:res[content-encoding]];'),
-    { skip: function (req, res) {
-        var CT = res.getHeader('content-type')
-        if (!CT) return true;
-        return CT.substring(0, 4) !== 'text' }, stream: sdchLog }
+    { skip: function (req, res) { return !isTextContent(res) }, stream: sdchLog }
 ))
 
 app.use(connectSdch.compress({ threshold: '1kb' }, { /* some zlib options */ }));
@@ -98,8 +100,15 @@ app.get('/*', function proxy(req, res, next) { // get по любому url
             }
             p.pipe(res);
             var parseUrl = url.parse(req.url)
-            if (config.domains.indexOf(parseUrl.hostname) != -1) {
-                var dir = config.dictionaryRootdir + '/' + parseUrl.hostname
+            var currDomain =  getDomain(parseUrl.hostname)
+            var domainNum = -1
+            for(var i = 0; i < config.domains.length; i++) {
+                if (config.domains[i].domainName == currDomain) {
+                    domainNum = i
+                }
+            }
+            if (domainNum != -1 && isTextContent(res)) {
+                var dir = config.dictionaryRootdir + '/' + currDomain
                 mkdirp(dir, function (err) {
                     if (!err) {
                         p.pipe(fs.createWriteStream(dir + '/'
@@ -111,6 +120,24 @@ app.get('/*', function proxy(req, res, next) { // get по любому url
                         console.log(err);
                     }
                 });
+                domainHits[domainNum] += 1
+                if (domainHits[domainNum] == config.domains[domainNum].domainPageInDict) {
+                    var child = exec('./' + config.dictionaryGenerator + ' ' + currDomain,
+                        function (error, stdout, stderr) {
+                            //TODO  перегрузить словарь для домена из этого файла
+                            console.log('stdout: ' + stdout);
+                            fs.readFile(stdout.replace(/\n/, ''), function (err, data) {
+                                if (err) throw err;
+                                dicts[0].url = 'http://' + currDomain + '/dictionaries/dict-x' + randWD(13)
+                                dicts[0].domain = currDomain
+                                dicts[0].data = data
+                            });
+                            if (error !== null) {
+                                console.log('exec error: ' + error);
+                            }
+                        });
+                    domainHits[domainNum] = 0
+                }
             }
         }).end();
 });
@@ -153,8 +180,29 @@ if (module.parent) {
     run()
 }
 
-function fixedEncodeURIComponent (str) {
+function fixedEncodeURIComponent(str) {
     return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
         return '%' + c.charCodeAt(0).toString(16);
     });
 }
+
+function getDomain(hostName) {
+    var domain = hostName;
+    if (hostName != null) {
+        var parts = hostName.split('.').reverse();
+        if (parts != null && parts.length > 1) {
+            domain = parts[1] + '.' + parts[0];
+        }
+    }
+    return domain;
+}
+
+function isTextContent(res) {
+    var CT = res.getHeader('content-type')
+    if (!CT) return false;
+    return CT.substring(0, 4) === 'text'
+}
+
+function randWD(n){  // random words and digits
+    return Math.random().toString(36).slice(2, 2 + Math.max(1, Math.min(n, 10)) );
+} //result is such as "46c17fkfpl"
